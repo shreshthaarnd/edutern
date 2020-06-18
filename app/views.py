2986ed90d820f5c4a0cc667c29ae333e24d2473c
unknown <sonalikul1999@gmail.com>
2020-06-19 00:09:34 +0530

    sonali commit2

---
+++ app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+	return render(request,'index.html',{})
+def aboutus(request):
+	return render(request,'about-us.html',{})
+def blog(request):
+	return render(request,'blog.html',{})
+def contact(request):
+	return render(request,'contact.html',{})
+def coursedetails(request):
+	return render(request,'course-details.html',{})
+def courses(request):
+	return render(request,'courses.html',{})
+def elements(request):
+	return render(request,'elements.html',{})
+def singleblog(request):
+	return render(request,'single-blog.html',{})
+def adminindex(request):
+	return render(request,'adminpages/index.html',{})
+def adminlogin(request):
+	return render(request,'adminpages/login.html',{})
+def admincodorderlist(request):
+	return render(request,'adminpages/codorderlist.html',{})
