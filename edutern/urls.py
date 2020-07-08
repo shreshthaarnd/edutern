54cb2ddf63647ffcb6b2f8f7be4f7e5bb05fad7e
aryankul97 <aryankul97@gmail.com>
2020-07-08 16:51:47 +0530

    aryan commit

--- edutern/urls.py
+++ edutern/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/',index),
+    path('',index),
     path('aboutus/',aboutus),
     path('blog/',blog),
     path('contact/',contact),
